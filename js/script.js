bbd2d89e05528393c7454aa55cb2324c1c876e24
Soniro <soniro271@gmail.com>
2022-01-16 17:42:59 +0200

    new lesson

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privet: false
-};
-
-const a = prompt('Один из последних фильмов?', ''),
-    b = prompt('На сколько оцените его?', ),
-    с = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[с] = d;
-
-console.log(personalMovieDB);
